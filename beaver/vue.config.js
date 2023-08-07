01fd3f06a5feccc87c8728fc1766e75de7e3c712
sub <s3238s@naver.com>
2023-08-07 13:10:27 +0900

    spring boot 재설정

--- beaver/vue.config.js
+++ beaver/vue.config.js
@@ -1,8 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: '../vue-back/vue-back/src/main/resources/static', // Build Directory
+  outputDir: '../backend/src/main/resources/static', // Build Directory
 	devServer: {
-		proxy: 'http://localhost' // Spring Boot Server
+		proxy: {
+			'/api' : {
+				target : 'http://localhost:8787', // Spring Boot Server
+				changeOrigin: true // cross origin 허용
+			}
+		}
 	}
 })
